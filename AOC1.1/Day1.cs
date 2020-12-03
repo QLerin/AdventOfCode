8ceb98c7f4ea4d01ba366af87bf1b74b71d9ee6f
Linas Krašauskas <linask03@gmail.com>
2020-12-03 23:56:25 +0200

    Slight clean-up.

---
+++ AOC1.1/Day1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace AOC1._1
+{
+    public class Day1
+    {
+        public static void Task1()
+        {
+            //TODO replace with relative call
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data1.1.txt");
+            var numbers = lines.Select(line => int.Parse(line)).ToList();
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                for (int x = 1; x < numbers.Count; x++)
+                {
+                    if (numbers[x] + numbers[i] == 2020)
+                    {
+                        var multiplication = numbers[x] * numbers[i];
+                        Console.WriteLine($"Day 1, task 1: {multiplication}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        public static void Task2()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data1.2.txt");
+            var numbers = lines.Select(line => int.Parse(line)).ToList();
+
+            for (int i = 0; i < numbers.Count - 2; i++)
+            {
+                for (int x = 1; x < numbers.Count - 1; x++)
+                {
+                    for (int y = 2; y < numbers.Count; y++)
+                    {
+                        if (numbers[x] + numbers[i] + numbers[y] == 2020)
+                        {
+                            var multiplication = numbers[x] * numbers[i] * numbers[y];
+                            Console.WriteLine($"Day 1, task 2: {multiplication}");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AOC1.1/Day3.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace AOC1._1
+{
+    public class Day3
+    {
+        public static void Task1()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data3.1.txt");
+            int treesCount = 0;
+            int startingX = 0;
+            int rowsCount = lines.First().Length;
+            foreach (var line in lines)
+            {
+                var charArray = line.ToCharArray();
+
+                if (startingX >= rowsCount)
+                {
+                    startingX -= rowsCount;
+                }
+
+                if (charArray[startingX] == '#')
+                {
+                    treesCount++;
+                }
+                startingX += 3;
+            }
+            Console.WriteLine($"Day 3, task 1: {treesCount}");
+        }
+
+        public static void Task2()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data3.2.txt");
+            int treesCount11 = GetTreesCount(lines, 1, 1);
+            int treesCount31 = GetTreesCount(lines, 3, 1);
+            int treesCount51 = GetTreesCount(lines, 5, 1);
+            int treesCount71 = GetTreesCount(lines, 7, 1);
+            int treesCount12 = GetTreesCount(lines, 1, 2);
+            var multiplication = treesCount11 * treesCount31 * treesCount51 * treesCount71 * treesCount12;
+            Console.WriteLine($"Day 3, task 2: {multiplication}");
+        }
+
+        private static int GetTreesCount(string[] lines, int xIncrement, int yIncrement)
+        {
+            int rowsCount = lines.First().Length;
+            int startingX = 0;
+            int treesCount = 0;
+            for (int i = 0; i < lines.Length; i += yIncrement)
+            {
+                var charArray = lines[i].ToCharArray();
+
+                if (startingX >= rowsCount)
+                {
+                    startingX -= rowsCount;
+                }
+
+                if (charArray[startingX] == '#')
+                {
+                    treesCount++;
+                }
+
+                startingX += xIncrement;
+            }
+
+            return treesCount;
+        }
+    }
+}
