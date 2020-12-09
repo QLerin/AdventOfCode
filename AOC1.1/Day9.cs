871ad804f66a091e369386818d851fab60c6dade
Linas Krašauskas <linask03@gmail.com>
2020-12-09 12:54:53 +0200

    Day 9, part 2.

--- AOC1.1/Day9.cs
+++ AOC1.1/Day9.cs
@@ -10,8 +10,44 @@ namespace AOC1._1
         {
             string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data9.txt");
             var numbers = lines.Select(line => long.Parse(line)).ToList();
-            var preamble = 25;
+            long invalidNumber = GetInvalidNumber(numbers);
+            Console.WriteLine($"Day 9, task 1: {invalidNumber}");
+        }
 
+        public static void Task2()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data9.txt");
+            var numbers = lines.Select(line => long.Parse(line)).ToList();
+            long invalidNumber = GetInvalidNumber(numbers);
+
+            var invalidIndex = numbers.IndexOf(invalidNumber);
+
+            for (var i = 0; i < invalidIndex; i++)
+            {
+                long sum = 0;
+                var sumNumbers = new List<long>();
+                for (var j = i; j < invalidIndex; j++)
+                {
+                    sum += numbers[j];
+                    sumNumbers.Add(numbers[j]);
+                    if (sum > invalidNumber)
+                    {
+                        break;
+                    }
+
+                    if (invalidNumber == sum)
+                    {
+                        Console.WriteLine($"Day 9, task 2: {sumNumbers.Min() + sumNumbers.Max()}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static long GetInvalidNumber(List<long> numbers)
+        {
+            var preamble = 25;
+            var invalidNumber = long.MinValue;
             for (int i = preamble; i < numbers.Count; i++)
             {
                 var sums = new HashSet<long>();
@@ -29,14 +65,13 @@ namespace AOC1._1
                 }
                 if (!sums.Contains(numbers[i]))
                 {
-                    Console.WriteLine($"Day 9, task 1: {numbers[i]}");
+                    invalidNumber = numbers[i];
+
                     break;
                 }
             }
-        }
 
-        public static void Task2()
-        {
+            return invalidNumber;
         }
     }
 }
