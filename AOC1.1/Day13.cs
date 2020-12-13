c5cf6e9c7e652a09151f2e238f0f5164fcd8b9a3
Linas Krašauskas <li.nask03@gmail.com>
2020-12-13 21:50:14 +0200

    Day 13, part 1.

--- AOC1.1/Day13.cs
+++ AOC1.1/Day13.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace AOC1._1
 {
@@ -7,7 +8,13 @@ namespace AOC1._1
         public static void Task1()
         {
             string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data13.txt");
-            //Console.WriteLine($"Day 13, task 1: {lines}");
+
+            var earliestArival = int.Parse(lines[0]);
+            var buses = lines[1].Split(",").Where(text => text != "x").Select(number => int.Parse(number)).ToList();
+            var waitTimes = buses.Select(bus => (bus, bus - earliestArival % bus)).ToList();
+
+            var minValue = waitTimes.Min(waitTime => waitTime.Item2);
+            Console.WriteLine($"Day 13, task 1: {waitTimes[6].Item2 * waitTimes[6].bus}");
         }
 
         public static void Task2()
