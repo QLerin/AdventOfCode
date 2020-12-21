0c2514f56d7d4680bbbd132dea19674ebac8cd82
Linas Krašauskas <li.nask03@gmail.com>
2020-12-21 19:10:20 +0200

    Day 21, part 1.

--- AOC1.1/Day21.cs
+++ AOC1.1/Day21.cs
@@ -1,18 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AOC1._1
 {
     public class Day21
     {
+        public class AllergenWithPossibilities
+        {
+            public AllergenWithPossibilities(string name, HashSet<string> possibilities)
+            {
+                Name = name;
+                Possibilities = possibilities;
+            }
+
+            public string Name { get; }
+            public HashSet<string> Possibilities { get; }
+        }
+
         public static void Task1()
         {
             string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data21.txt");
-            //Console.WriteLine($"Day 21, task 1: {lines}");
+            var allergensWithPossibilities = new List<AllergenWithPossibilities>();
+            var allPossibilities = new List<string>();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(" (contains ");
+                var possibilities = parts[0].Split(" ");
+                allPossibilities.AddRange(possibilities);
+                var allergens = parts[1].Substring(0, parts[1].Length - 1).Split(", ");
+                allergensWithPossibilities.AddRange(allergens.Select(allergen => new AllergenWithPossibilities(allergen, possibilities.ToHashSet())));
+            }
+
+            var groupedAllergens = allergensWithPossibilities.GroupBy(allergen => allergen.Name);
+            foreach (var grouped in groupedAllergens)
+            {
+                var inEveryGroup = allPossibilities.Distinct().Where(possibility => grouped.All(group => group.Possibilities.Contains(possibility)));
+                allPossibilities.RemoveAll(possibility => inEveryGroup.Contains(possibility));
+            }
+
+            Console.WriteLine($"Day 21, task 1: {allPossibilities.Count}");
         }
 
         public static void Task2()
         {
-
         }
     }
 }
