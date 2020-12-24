c348751b708d84a7f009abb0aebe8def909e0d82
Linas Krašauskas <li.nask03@gmail.com>
2020-12-24 14:45:44 +0200

    Day 21, part 2.

--- AOC1.1/Day21.cs
+++ AOC1.1/Day21.cs
@@ -22,6 +22,13 @@ namespace AOC1._1
         {
             string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data21.txt");
             var allergensWithPossibilities = new List<AllergenWithPossibilities>();
+            var withoutAllergens = GetWithoutAllergens(lines, allergensWithPossibilities);
+
+            Console.WriteLine($"Day 21, task 1: {withoutAllergens.Count}");
+        }
+
+        private static List<string> GetWithoutAllergens(string[] lines, List<AllergenWithPossibilities> allergensWithPossibilities)
+        {
             var allPossibilities = new List<string>();
 
             foreach (var line in lines)
@@ -30,21 +37,66 @@ namespace AOC1._1
                 var possibilities = parts[0].Split(" ");
                 allPossibilities.AddRange(possibilities);
                 var allergens = parts[1].Substring(0, parts[1].Length - 1).Split(", ");
-                allergensWithPossibilities.AddRange(allergens.Select(allergen => new AllergenWithPossibilities(allergen, possibilities.ToHashSet())));
+                allergensWithPossibilities.AddRange(allergens.Select(allergen =>
+                    new AllergenWithPossibilities(allergen, possibilities.ToHashSet())));
             }
 
             var groupedAllergens = allergensWithPossibilities.GroupBy(allergen => allergen.Name);
             foreach (var grouped in groupedAllergens)
             {
-                var inEveryGroup = allPossibilities.Distinct().Where(possibility => grouped.All(group => group.Possibilities.Contains(possibility)));
+                var inEveryGroup = allPossibilities.Distinct()
+                    .Where(possibility => grouped.All(group => @group.Possibilities.Contains(possibility)));
                 allPossibilities.RemoveAll(possibility => inEveryGroup.Contains(possibility));
             }
 
-            Console.WriteLine($"Day 21, task 1: {allPossibilities.Count}");
+            return allPossibilities;
         }
 
         public static void Task2()
         {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Remote\AdventOfCoding2020\AOC1.1\Resources\Data21.txt");
+            var allergens = GetAllergensList(lines).ToHashSet();
+
+            var allergensWithPossibilities = new List<AllergenWithPossibilities>();
+            
+            var groupedAllergens = allergens.GroupBy(allergen => allergen.Name);
+            foreach (var grouped in groupedAllergens)
+            {
+                var possibilities = grouped.SelectMany(group => group.Possibilities).ToHashSet();
+                possibilities = possibilities.Where(possibility => grouped.All(group => group.Possibilities.Contains(possibility))).ToHashSet();
+                allergensWithPossibilities.Add(new AllergenWithPossibilities(grouped.Key, possibilities));
+            }
+
+            var tuples = new List<(string name, string otherName)>();
+            while (true)
+            {
+                var onePossibility = allergensWithPossibilities.FirstOrDefault(allergen => allergen.Possibilities.Count == 1);
+                if (onePossibility == null)
+                {
+                    break;
+                }
+                tuples.Add((onePossibility.Name, onePossibility.Possibilities.First()));
+                allergensWithPossibilities.Remove(onePossibility);
+                allergensWithPossibilities.ForEach(allergen => allergen.Possibilities.Remove(onePossibility.Possibilities.First()));
+            }
+
+            var result = string.Join(',', tuples.OrderBy(tuple => tuple.name).Select(tuple => tuple.otherName));
+            Console.WriteLine($"Day 21, task 2: {result}");
+        }
+
+        private static List<AllergenWithPossibilities> GetAllergensList(string[] lines)
+        {
+            var allergensList = new List<AllergenWithPossibilities>();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(" (contains ");
+                var possibilities = parts[0].Split(" ");
+                var allergens = parts[1].Substring(0, parts[1].Length - 1).Split(", ");
+                allergensList.AddRange(allergens.Select(allergen => new AllergenWithPossibilities(allergen, possibilities.ToHashSet())));
+            }
+
+            return allergensList;
         }
     }
 }
